c0c53e1a570e41646d1e03dab5df574420e8b964
isabarrenha <barrenha95@yahoo.com.br>
2025-08-27 07:09:27 -0300

    ml-flow-first-configs

--- training/model_training.py
+++ training/model_training.py
@@ -31,12 +31,18 @@ import pandas as pd
 import numpy  as np
 import joblib
 import os
+import mlflow
+from itertools import product
 from feature_store.feature_engineering  import (load_data, clean_data, engineering)
 from feature_store.feature_store  import (save_offline, load_offline)
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn import tree
+from sklearn.model_selection import GridSearchCV
 
+
+mlflow.set_tracking_uri('http://localhost:5000') # Set which mlflow server will use
+mlflow.set_experiment(experiment_id=620958864307953100) # Set Experiment
 
 
 # =========================
@@ -52,10 +58,54 @@ def load_feature_store_data(filepath: str) -> pd.DataFrame:
     print(df)
     
 
+def train_model(X_train: pd.DataFrame
+              , X_test: pd.DataFrame
+              , y_train: pd.DataFrame
+              , y_test: pd.DataFrame) -> None:
+    """Train a DecisionTreeClassifier with different hyperparameters,
+    logging each run to MLflow."""
 
-def train_model(df: pd.DataFrame) -> pd.DataFrame:
-    """Clean raw data (remove duplicates, handle missing values)."""
     
+    # Define parameter grid
+    param_grid = {
+        "min_samples_leaf": [10, 20, 30, 50, 100],
+        "min_samples_split": [2, 5, 10]
+    }
+
+    for params in product(param_grid["min_samples_leaf"], param_grid["min_samples_split"]):
+        min_samples_leaf, min_samples_split = params
+
+        with mlflow.start_run: # Used to save runs in the experiment
+
+            mlflow.sklearn.autolog() # Tell mlflow to generate some logs of the run
+
+            # Log parameters manually
+            mlflow.log_params({
+                "min_samples_leaf": min_samples_leaf,
+                "min_samples_split": min_samples_split
+            })
+
+            # Define and fit model
+            clf = tree.DecisionTreeClassifier(
+                min_samples_leaf=min_samples_leaf,
+                min_samples_split=min_samples_split,
+                random_state=42
+            )
+            
+            clf.fit = (X_train, y_train)
+            y_train_predict = clf.predict(X_train)
+            y_test_predict  = clf.predict(X_test)
+
+
+            acc_train = metrics.accuracy_score(y_train, y_train_predict)
+            acc_test  = metrics.accuracy_score(y_test , y_test_predict)
+
+            mlflow.log_metrics({
+                "acc_train": acc_train,
+                "acc_test": acc_test
+            })
+
+        
 # =========================
 # Standalone Script (Testing)
 # =========================
